582f9c09bdde1953a0ee0700ab8ada6a9998f605
ZaiquiriW <zaiq@pop-os.localdomain>
2023-05-05 14:17:58 -0500

    Pushing docker compose template

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker Compose!
+          Testing Dev Change using Docker Volume
         </h1>
 
         <p className={styles.description}>
