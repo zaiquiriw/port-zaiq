9ab04beabea830b456b4545e0e4f2b5a223725b3
zaiquiriw <zaiquiriw@gmail.com>
2023-05-06 23:41:24 -0500

    I need to setup MobaXTerm

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo "Reached Port Zaiquiri"
-echo "Current directory: ${pwd}"
+echo "Current directory: $(pwd)"
 docker load -i images.tar.gz
 
 echo "Create network if it doesn't exist"
 docker network ls | grep dock-network || docker network create dock-network
 
 echo 'Create containers with existing images'
-docker compose -f $DOCKER_COMPOSE_FILE up -d
+echo "Running: docker compose -f $(DOCKER_COMPOSE_FILE) up -d"
+docker compose -f $DOCKER_COMPOSE_FILE up --detach
